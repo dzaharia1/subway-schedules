0d97878aa74336faa7f2d1bdd61f4c2a1eea8b46
Dan Zaharia <zaharia.danny@gmail.com>
2022-07-23 10:42:19 -0400

    rewrite trip updates call to take an array of services to query

--- modules/gtfs.js
+++ modules/gtfs.js
@@ -3,14 +3,17 @@ const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 const https = require('https');
 const request = require('request');
 const fs = require('fs');
+const cons = require('consolidate');
 
 let stationsFile = path.join(__dirname, '../agencyInfo/stations.txt');
 let routesFile =  path.join(__dirname, '../agencyInfo/routes.txt');
 
-let tripUpdates = [];
+// let tripUpdates = [];
 let arrivals = [];
 let routes = [];
 let stations = [];
+
+let services = [ "ACE", "BDFM", "G", "JZ", "NQRW", "L", "1234566X7", "SI" ];
 
 let feeds = {
     "ACE": "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace",
@@ -19,9 +22,8 @@ let feeds = {
     "JZ": "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-jz",
     "NQRW": "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-nqrw",
     "L": "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l",
-    "1234567": "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs",
+    "1234566X7": "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs",
     "SI": "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-si"
-
 }
 
 const requestSettings = {
@@ -34,23 +36,32 @@ const requestSettings = {
 
 setUpStations();
 setUpRoutes();
-getStationSchedule("A24", "ACE");
+// // getStationSchedule("A24", "ACE");
+// getStationSchedule("A24");
 
 function setUpStations() {
     console.log('Setting up stations');
     fs.readFile(stationsFile, {encoding: 'utf8'}, (err, data) => {
         if (!err) {
             let rawData = CSVToArray(data, ',');
-            for (station of rawData) {
-                let stationObject = {};
-                stationObject.stopId = station[0];
-                stationObject.name = station [1];
-                stationObject.lines = []
-                for (let i = 2; i < station.length; i ++) {
-                    stationObject.lines.push(station[i].replace(/\s/g, ''));
+            for (let station of rawData) {
+                let existingStation = stations.find(obj => obj.name.indexOf(station[1]) > -1);
+                if (existingStation) {
+                    for (let i = 2; i < station.length; i ++) {
+                        if (station[i][0] === ' ') { station[i] = station[i].substr(1) }
+                        existingStation.lines.push(station[i]);
+                    }
+                } else {
+                    let stationObject = {};
+                    stationObject.stopId = station[0];
+                    stationObject.name = station[1];
+                    stationObject.lines = []
+                    for (let i = 2; i < station.length; i ++) {
+                        stationObject.lines.push(station[i].replace(/\s/g, ''));
+                    }
+                    stationObject.lines.sort((a, b) => (a > b) ? 1 : -1);
+                    stations.push(stationObject);
                 }
-                stationObject.lines.sort((a, b) => (a > b) ? 1 : -1);
-                stations.push(stationObject);
             }
         } else {
             console.error(err);
@@ -58,12 +69,26 @@ function setUpStations() {
     });
 }
 
+function getFeedsForStation(stopId) {
+    let returnArray = [];
+    let thisStation = stations.find(obj => obj.stopId.indexOf(stopId) > -1);
+
+    for (let line of thisStation.lines) {
+        let lineService = services.find(obj => obj.indexOf(line[0]) > -1 );
+        if (!returnArray.includes(lineService)) {
+            returnArray.push(lineService);
+        }
+    }
+
+    return returnArray;
+}
+
 function setUpRoutes() {
     console.log('Setting up routes');
   fs.readFile(routesFile, {encoding: 'utf8'}, (err, data) => {
     if (!err) {
         let rawData = CSVToArray(data, ',');
-        for (route of rawData) {
+        for (let route of rawData) {
             let routeObject = {};
             routeObject.routeId = route[1];
             routeObject.routeName = route[2];
@@ -79,54 +104,93 @@ function setUpRoutes() {
   });
 }
 
-function getTripUpdates (service, callback) {
-    tripUpdates = [];
+function getTripUpdates (services, tripUpdatesArray, callback) {
+    console.log(`getting updates for`);
+    console.log(services);
+    service = services[0];
     requestSettings.url = feeds[service];
     request(requestSettings, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             let gtfsData = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
-            for (entity of gtfsData.entity) {
-                if (entity.tripUpdate) { tripUpdates.push(entity) }
+            for (let entity of gtfsData.entity) {
+                if (entity.tripUpdate) { tripUpdatesArray.push(entity); }
             }
-            callback();
+            if (services.length > 1) {
+                services.shift();
+                console.log(`New services array is ${services}`);
+                getTripUpdates(services, tripUpdatesArray, callback);
+            } else {
+                callback();
+            }
         } else {
-            console.error(error);
+            console.log(error);
         }
     });
 }
 
-function getStationSchedule(stopId, service, callback) {
-    console.log(`Getting ${service} schedule for ${stopId}`);
-    arrivals = [];
+// function getTripUpdates (service, callback) {
+//     let tripUpdates = [];
+//     requestSettings.url = feeds[service];
+//     request(requestSettings, (error, response, body) => {
+//         if (!error && response.statusCode == 200) {
+//             let gtfsData = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+//             for (let entity of gtfsData.entity) {
+//                 if (entity.tripUpdate) { tripUpdates.push(entity) }
+//             }
+//             callback(tripUpdates);
+//         } else {
+//             console.error(error);
+//         }
+//     });
+// }
 
-    getTripUpdates(service, () => {
-        for (tripUpdate of tripUpdates) {
-            for (stopTimeUpdate of tripUpdate.tripUpdate.stopTimeUpdate) {
-                if (stopTimeUpdate.stopId.indexOf(stopId) >= 0){
-                    let scheduleItem = {};
-                    let tripId = tripUpdate.tripUpdate.trip.tripId;
-                    let timeStamp = parseInt(stopTimeUpdate.arrival.time.low) * 1000;
-                    let arrivalTime = new Date(timeStamp);
-                    let now = Date.now();
-                    let minutesUntil = Math.floor((timeStamp - now) / 60000);
-                    scheduleItem.arrivalTime = `${arrivalTime.getHours()}:${arrivalTime.getMinutes()}`;
-                    scheduleItem.routeId = tripUpdate.tripUpdate.trip.routeId;
-                    scheduleItem.minutesUntil = minutesUntil;
-                    scheduleItem.direction = tripId.substring(tripId.length - 1, tripId.length);
-                    if (scheduleItem.minutesUntil >= 0) {
-                        arrivals.push(scheduleItem);
-                    }
-                }
-            }
-        }
-        arrivals.sort((a, b) => (a.minutesUntil > b.minutesUntil) ? 1 : -1);
-        exports.arrivals = arrivals;
-        if (callback) { callback() }
+function getStationSchedule(stopId, callback) {
+    // get the trip updates for each of the services of the station
+    let stationServices = getFeedsForStation(stopId);
+    let tripUpdates = [];
+    let arrivals = [];
+    getTripUpdates(stationServices, tripUpdates, () => {
+        console.log(tripUpdates);
     });
+    // sort the trip updates
+    // prepare the schedule array
+    // put the array through the callback function
 }
+
+// ~~~~~~~~~~ Get the schedule for a service of a station ~~~~~~~~~~~~~
+
+// function getStationSchedule(stopId, service, callback) {
+//     console.log(`Getting ${service} schedule for ${stopId}`);
+//     arrivals = [];
+
+//     getTripUpdates(service, () => {
+//         for (let tripUpdate of tripUpdates) {
+//             for (let stopTimeUpdate of tripUpdate.tripUpdate.stopTimeUpdate) {
+//                 if (stopTimeUpdate.stopId.indexOf(stopId) >= 0){
+//                     let scheduleItem = {};
+//                     let tripId = tripUpdate.tripUpdate.trip.tripId;
+//                     let timeStamp = parseInt(stopTimeUpdate.arrival.time.low) * 1000;
+//                     let arrivalTime = new Date(timeStamp);
+//                     let now = Date.now();
+//                     let minutesUntil = Math.floor((timeStamp - now) / 60000);
+//                     scheduleItem.arrivalTime = `${arrivalTime.getHours()}:${arrivalTime.getMinutes()}`;
+//                     scheduleItem.routeId = tripUpdate.tripUpdate.trip.routeId;
+//                     scheduleItem.minutesUntil = minutesUntil;
+//                     scheduleItem.direction = tripId.substring(tripId.length - 1, tripId.length);
+//                     if (scheduleItem.minutesUntil >= 0) {
+//                         arrivals.push(scheduleItem);
+//                     }
+//                 }
+//             }
+//         }
+//         arrivals.sort((a, b) => (a.minutesUntil > b.minutesUntil) ? 1 : -1);
+//         exports.arrivals = arrivals;
+//         if (callback) { callback() }
+//     });
+// }
 
 function getVehicleFromTripID (tripId) {
-  for (vehicle of vehicles) {
+  for (let vehicle of vehicles) {
     if (vehicle.trip.tripId.indexOf(tripId) >= 0) {
       return vehicle;
     }
@@ -167,7 +231,7 @@ function CSVToArray( strData, strDelimiter ){
     return( arrData );
 }
 
-exports.arrivals = arrivals;
+// exports.arrivals = arrivals;
 exports.stations = stations;
 exports.routes = routes;
 exports.setUpStations = setUpStations;

c4689a52cd270e1ebdc359d7ef32dea9b7874903
Dan Zaharia <zaharia.danny@gmail.com>
2022-07-25 23:31:55 -0400

    allow user to select multiple stations to track

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -3,16 +3,38 @@ let arrivalItems;
 let stationSelectorItems;
 let stationSelector;
 let stationSearch;
+let stationsSubmit;
+let selectionIndicator;
+let stationName;
 
 var readyFunction = function() {
 	stopSelector = document.querySelector('#stop-selector');
 	arrivalItems = document.querySelectorAll('.arrival');
-	stationSelector = document.querySelector('.station-selector');
+	stationSelector = document.querySelector('.station-selector__list');
 	stationSelectorItems = document.querySelectorAll('.station-selector__station');
 	stationSearch = document.querySelector('.station-selector__search-input');
+	stationName = stationSearch.placeholder;
+	stationsSubmit = document.querySelector('.station-selector__submit');
+	selectionIndicator = document.querySelector('.station-selector__selection-indicator');
 
 	for (let station of stationSelectorItems) {
 		station.parentNode.removeChild(station);
+		let stopid = station.getAttribute(`data-stopid`);
+		let checkbox = station.querySelector('input');
+		
+		station.addEventListener('click', (e) => {
+			let checkbox = station.querySelector('input[type="checkbox"]');
+			if (checkbox.checked) {
+				checkbox.checked = false;
+				let indicator = selectionIndicator.querySelector(`[data-stopid="${stopid}"]`);
+				indicator.parentNode.removeChild(indicator);
+				stationsSubmit.href.replace(`/${stopid}`, '');
+			} else {
+				setSelectedStationIndicator(station);
+				stationsSubmit.href += `/${stopid}`;
+				checkbox.checked = true;
+			}
+		});
 	}
 
 	for (arrival of arrivalItems) {
@@ -27,24 +49,61 @@ var readyFunction = function() {
 		stationSearch.placeholder = '';
 	});
 
+	stationSearch.addEventListener('blur', (e) => {
+		if (stationSearch.value === '') {
+			stationSearch.placeholder = stationName;
+			stationSearch.value = '';
+		}
+	});
+
 	stationSearch.addEventListener('input', (e) => {
 		let searchTerm = stationSearch.value.toLowerCase();
 		stationSelector.innerHTML = '';
-		console.log(`Searching for ${searchTerm}`);
+
 		for (let station of stationSelectorItems) {
 			let stationName = station.getAttribute('data-name').toLowerCase();
 			if (stationName.indexOf(searchTerm) > -1) {
-				console.log(stationName);
 				stationSelector.appendChild(station);
 			}
 		}
 
-		if (stationSelector.innerHTML != '') {
-			stationSelector.style.display = 'flex';
+		if (stationSelector.innerHTML != '' || selectionIndicator.innerHTML != '') {
+			stationSelector.parentNode.style.display = 'flex';
 		} else {
-			stationSelector.style.display = 'none';
+			stationSelector.parentNode.style.display = 'none';
 		}
 	});
+}
+
+function setSelectedStationIndicator(station) {
+	let newNode = document.createElement('button');
+	let name = station.getAttribute('data-name');
+	let stopid = station.getAttribute('data-stopid');
+	let lines = station.querySelectorAll('.station-selector__station-line');
+	newNode.classList.add('station-selector__selection-indicator-item')
+
+	newNode.setAttribute('data-name', name);
+	newNode.setAttribute('data-stopid', stopid);
+	newNode.innerHTML = `<p>${name}</p>`;
+	for (let line of lines) {
+		newNode.innerHTML += `<span line="${line.innerText}">${line.innerText}</span>`
+	}
+
+	newNode.addEventListener('click', (e) => {
+		let stationSelectorItem = getStationItem(stopid);
+		stationSelectorItem.querySelector('input').checked = false;
+		newNode.parentNode.removeChild(newNode);
+	});
+
+	selectionIndicator.appendChild(newNode);
+}
+
+function getStationItem(stopid) {
+	for (let item of stationSelectorItems) {
+		if (item.getAttribute('data-stopid') === stopid) {
+			return item;
+		}
+	}
 }
 
 if (document.readyState != 'loading') {

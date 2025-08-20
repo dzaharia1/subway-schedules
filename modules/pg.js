872bac41d316e00e509e328c4017416ebc07b087
Dan Zaharia <zaharia.danny@gmail.com>
2025-08-20 10:03:58 -0400

    SSL setting on databse connection should be conditional upon the environment

--- modules/pg.js
+++ modules/pg.js
@@ -1,9 +1,11 @@
 const { Pool } = require('pg');
 
 // Use connection pooling instead of a single client
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: false,
+    ssl: isProduction ? { rejectUnauthorized: false } : false,
     max: 20, // Maximum number of clients in the pool
     idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
     connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established

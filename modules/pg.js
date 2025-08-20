4df2aa2c2980e066d8364a8c89440dadfd587df8
Dan Zaharia <zaharia.danny@gmail.com>
2025-08-20 09:57:00 -0400

    simplify and correct connection string

--- modules/pg.js
+++ modules/pg.js
@@ -2,10 +2,8 @@ const { Pool } = require('pg');
 
 // Use connection pooling instead of a single client
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://dan@localhost:5432/subway-sign',
-    ssl: process.env.DATABASE_URL ? {
-        rejectUnauthorized: false
-    } : false,
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
     max: 20, // Maximum number of clients in the pool
     idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
     connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
